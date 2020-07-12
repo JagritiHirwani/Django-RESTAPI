6e67861ae44b8e9bf735f31cb9a41cc6666cf341
jagriti hirwani <jagritihirwani@MacBook-Pro.local>
2020-07-12 18:01:29 +0530

    Enabled the Django admin for the user profile model

--- src/restapi_project/profiles_api/models.py
+++ src/restapi_project/profiles_api/models.py
@@ -22,10 +22,10 @@ class UserProfileManager(BaseUserManager):
      def  create_superuser(self,email,name,password):
          """Creates and saves a new superuser with given details"""
 
-         user = self.create_user(email,name,psasword)
+         user = self.create_user(email,name,password)
          user.is_superuser = True
          user.is_staff = True
-         user.save(using = self._db)     
+         user.save(using = self._db)
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
